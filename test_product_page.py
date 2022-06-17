9cc65aa00ac7ff8c1ba316a8308bfef76bf6a447
BarGreen <satanarchist69@gmail.com>
2022-06-17 22:32:00 +0300

    -- add new BasePage logic, add 2 tests for ProductPage

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,10 @@
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 import pytest
 import time
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/",
+@pytest.mark.parametrize('link', [pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/", 
+                            marks=pytest.mark.xfail)])
                                 #   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                 #   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
                                 #   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
@@ -12,7 +14,7 @@ import time
                                 #   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
                                 #   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                 #   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
-                                  ])
+                                #   ])
 # def test_guest_can_add_product_to_basket(browser, link):
 #     page = ProductPage(browser, link)
 #     page.open()
@@ -33,10 +35,25 @@ def test_guest_cant_see_success_message(browser, link):
     page.open()
     page.should_not_be_success_message()
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"])
+@pytest.mark.parametrize('link', [pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/", 
+                            marks=pytest.mark.xfail)])
 def test_message_disappeared_after_adding_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
     # page.solve_quiz_and_get_code()
     page.should_disappeared_message()
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"])
+def test_guest_should_see_login_link_on_product_page(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"])
+def test_guest_can_go_to_login_page_from_product_page(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
